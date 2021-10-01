3945de1f5b00d903751973ba233eee3faee5978d
danushan99 <danuthasan08@gmail.com>
2021-10-02 00:21:49 +0530

    with error

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const String routeName = '/';
+  static Route route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => const HomeScreen(),
+    );
+  }
+
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
