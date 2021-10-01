c1c96510f1c5e02ab9c92826bf761b8d8db16aed
danushan99 <danuthasan08@gmail.com>
2021-10-01 15:37:04 +0530

    appBarUi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,11 +7,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'eshop_app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -24,6 +24,40 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      appBar: CustomAppBar(),
+      bottomNavigationBar: BottomAppBar(),
+    );
   }
+}
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.black,
+      elevation: 0,
+      title: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        color: Colors.black,
+        child: const Text(
+          'Shop App',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Avenir'),
+        ),
+      ),
+      iconTheme: const IconThemeData(color: Colors.pink),
+      actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.favorite))],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(50.0);
 }
